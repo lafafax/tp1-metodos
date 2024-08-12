b7f1fe9aee5f83991f63044471775280a04b7635
lafafax <142627356+lafafax@users.noreply.github.com>
2024-08-12 17:00:18 -0300

    Add files via upload

---
+++ metodos/ej2.js
@@ -0,0 +1,11 @@
+let arreglo=[];
+arreglo[0]="Lionel Messi";
+arreglo[1]="Angel Dimaria";
+arreglo[2]="Emiliano Martinez";
+arreglo[3]="Cristian Romero";
+arreglo[4]="Leandro Paredes";
+console.log(arreglo);
+
+arreglo.unshift("Lautaro Martinez");
+console.log(arreglo);
+arreglo.pop();
---
+++ metodos/ej5.js
@@ -0,0 +1,17 @@
+let arreglo=[];
+arreglo[0]=10;
+arreglo[1]=11;
+arreglo[2]=12;
+arreglo[3]=13;
+arreglo[4]=14;
+arreglo[5]=15;
+arreglo[6]=16;
+arreglo[7]=17;
+
+arreglo.shift();
+arreglo.shift();
+arreglo.shift();
+arreglo.shift();
+arreglo.shift();
+
+console.log(arreglo);
